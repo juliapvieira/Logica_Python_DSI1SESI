390f5533073915073614512106d0c51a323a3fe6
julia <julia.pardim@icloud.com>
2026-05-27 12:05:56 -0300

    Manipular

--- modulo_02/manipular_texto_empyhton..py
+++ modulo_02/manipular_texto_empyhton..py
@@ -62,3 +62,60 @@ print ("Do início ate 3:", st [:3])
 
 print ("Tamanho", len (st))
 
+# ------------------------------------
+# 5) OPERAÇÕES COM STRINGS
+# ------------------------------------
+# Python permite várias variações com strings
+# detectar se tem ou nao o "n" na palavra maracana, e vai detectar em booleano (verdadeiro ou falso)
+
+print ("n" in st)
+# Signfica que a letra "n" existe dentro da string
+
+print ("X" not in st)
+# Siginifica que "X" não existe na string
+
+print ("m" * 20)
+#Multiplicação repete a string
+
+print ("m" + "aracana " + texto1)
+# Operador + concatena strings
+
+# ---------------------------------------
+# 6) STRINGS SÃO IMUTÁVEIS
+# ---------------------------------------
+# Strings não podem ser alteradas diretamente!!!
+# Isso significa que o conteudo orginial não muda
+# O que acontece é a criação de uma nova string 
+
+# texto ="python 3"
+# python 3 = string
+
+# Método replace cria uma nova string
+# replace é substituir. 
+texto ="python 3"
+texto = texto.replace ("3", "2")
+
+print (texto)
+
+# -----------------------------------------
+# 7) MÉTODOS IMPORTANTES
+# -----------------------------------------
+# Strings possuem vários  métodos úteis.
+
+# isso serve para deixar a letra maiúscula
+cidade = "maracana" 
+print(cidade.capitalize())
+# Serve para colcoar a primeira letra em maiúscula
+
+# Conta quantas vezes "a" aparece
+print(cidade.count("a"))
+
+# Verifica se começa com "m"
+print(cidade.startswith("m"))
+
+# Verifica se termina com "z"
+print(cidade.endswith("z"))
+
+# divide a string em uma lista
+frase = "Copa de 2002"
+print(frase.split(" "))
